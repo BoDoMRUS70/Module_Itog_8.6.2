ad57368d9b1a591c98f4cd3f44b211ac0ce56103
Bodomrus70@mail.ru <Bodomrus70@mail.ru>
2022-12-15 17:25:46 +0700

    Добавьте файлы проекта.

---
+++ Module_Itog_8.6.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+
+namespace Module_8_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите путь к папке:");
+            string path = Console.ReadLine();
+            DirectoryInfo di = new DirectoryInfo(path);
+            if (di.Exists)
+            {
+                Console.WriteLine("Размер: " + DirectoryExtension.CountSize(di) + " байт");
+            }
+            else
+            {
+                Console.WriteLine("Папки не существует");
+            }
+        }
+    }
+}
